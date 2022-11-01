fc6cba89243c917b49217c635c4b7f92ce61115c
Someguy500 <92201578+Someguy500@users.noreply.github.com>
2022-11-01 22:59:39 +0800

    disconnected W/R Blend Tree, Added delay before new destination on patrol

--- Assets/Scripts/Enemy Related/EnemyAi/BehaviourTree/Patrol.cs
+++ Assets/Scripts/Enemy Related/EnemyAi/BehaviourTree/Patrol.cs
@@ -14,7 +14,10 @@ public class Patrol : Node
     private Animator _animator;
 
     [Header("Ind Var")]
-    private float patrolRad = 25f; 
+    private float patrolRad = 25f;
+    private bool stopped = false;
+    private float stoppedTime = 0;
+    private float stoppedMax = 8; 
 
     public Patrol(Transform transform, GameObject player, NavMeshAgent nva, EnemyScriptable ess, Animator animator)
     {
@@ -31,15 +34,30 @@ public class Patrol : Node
         if (d > 10)
         {
             Vector3 nextPos = Vector3.zero;
-            patrolRad = Random.Range(25, 50);
-            
-            if (_nva.remainingDistance <= _nva.stoppingDistance )
+            if(stopped == false)
             {
-                nextPos = PatrolPoint(patrolRad);
-                _nva.speed = _ess.Speed;
-                _nva.SetDestination(nextPos);
                 _animator.SetInteger("State", 1);
+                patrolRad = Random.Range(25, 50);
+                if (_nva.remainingDistance <= _nva.stoppingDistance)
+                {
+                    nextPos = PatrolPoint(patrolRad);
+                    _nva.speed = _ess.Speed;
+                    _nva.SetDestination(nextPos);
+                }
             }
+            else
+            {
+                if (stoppedTime <= 0)
+                {
+                    stopped = false;
+                    _animator.SetInteger("State", 0);
+                }
+                else
+                {
+                    stoppedTime -= Time.deltaTime;
+                }
+            }
+
             state = NodeState.RUNNING;
         }
         else
@@ -54,6 +72,8 @@ public class Patrol : Node
         Vector3 dir = Random.insideUnitSphere * distance;
         dir += _transform.position;
         NavMesh.SamplePosition(dir, out NavMeshHit navHit, distance, NavMesh.AllAreas);
+        stopped = true;
+        stoppedTime = stoppedMax;
         return navHit.position;
     }
 }
