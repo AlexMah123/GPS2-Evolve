143d04247bf280464d18b17a1a54c9cb044a36de
Zygody <shaneiskandar@gmail.com>
2022-11-30 01:57:29 +0800

    + Animator stuff to DeathState, some map updates

--- Assets/Scripts/Player Related/PlayerStateMachine/States/DeathState.cs
+++ Assets/Scripts/Player Related/PlayerStateMachine/States/DeathState.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 public class DeathState : PlayerStateMachine
 {
     public DeathState(PlayerController system) : base(system) { }
-    
+
     //There shouldnt be any code here unless theres something that the player can only do when dead
     //Just a dead end for the FSM to go to
-    
+    public override IEnumerator Start()
+    {
+        _system.animator.SetBool("Death", true);
+        yield break;
+    }
 }
